b2ea5344c0bcb12e79d2523071a9853143958eb8
Prejita Deepak <prejita.deepak@nafinc.com>
2023-05-21 11:17:59 +0530

    Add. State

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
+
+  test('renders "Hello World" as a text', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // ... nothing
+
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders "good to see" you if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  
